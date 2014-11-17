4da971d149098d6a79c225e98a634f18d8fdb0dd
hirerorninjas <info@rorninjas.com>
2014-11-17 15:04:39 +0530

    work on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,15 @@
     :last_name => "dealer",
     :dealer => "dealer"
 )
+
+ product = Product.create([
+  { name: "Maruti 800", category_id: "1",photo_file_name: "http://4.bp.blogspot.com/-2ajfPFLyauc/UhtTA04Ln5I/AAAAAAAABXE/QGZqtkY5bSk/s640/google-driverless-car.jpg", photo_content_type: "image/jpeg", photo_file_size: 68656, user_id: 2, price: 678908, quantity: 100, deal: false, category_name: nil },
+  { name: "Maruti 800", category_id: "2",photo_file_name: "http://www.ihdwal.com/wp-content/uploads/2013/05/White-Audi-Car-HD-Widescreen-Wallpaper.jpg", photo_content_type: "image/jpeg", photo_file_size: 68656, user_id: 2, price: 45672, quantity: 100, deal: false, category_name: nil }
+])
+
+category = Category.create([
+  { name: "Maruti"},
+  { name: "Mahindra"}
+])
+
+
