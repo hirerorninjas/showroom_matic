2587a19103fdae9292fcdf405ec2264d9ecef012
hirerorninjas <info@rorninjas.com>
2014-11-25 19:23:23 +0530

    work on template

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,16 @@
 class WelcomeController < ApplicationController
+	before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
   def index
-  	@products = Product.all
+  	if params[:search]
+      @products = Product.search(params[:search]).paginate(:per_page => 4, :page => params[:page])
+    else 
+      @products = Product.paginate(page: params[:page], per_page: 3) 
+      @like_ids = Like.all.collect(&:product_id)
+      @product_ids = Product.all.collect(&:id)
+      respond_with(@products)
+    end
   end
   
   def show_dealer
