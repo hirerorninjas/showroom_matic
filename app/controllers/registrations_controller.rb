c89d92689b92963b4ef57f6227b72b8c26425011
hirerorninjas <info@rorninjas.com>
2014-12-11 18:42:19 +0530

    worked on reg/edit page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -51,6 +51,6 @@ private
   end
  
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role_id, :resume )
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :role_id, :resume )
   end
 end
