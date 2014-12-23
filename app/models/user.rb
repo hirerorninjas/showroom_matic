f8c716b46a8e08f241115b1a00c5eadec3233bd1
hirerorninjas <info@rorninjas.com>
2014-12-23 16:08:13 +0530

    worked on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,21 @@ class User < ActiveRecord::Base
    has_many :products, through: :likes
   has_many :likes, dependent: :destroy
   has_many :salesmen
-#specify that the resume is a paperclip file attachment
+# specify that the resume is a paperclip file attachment
   has_attached_file :resume
   # validates_attachment_presence :resume
-  validates_attachment_content_type :resume, :content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document), 
-  :path => ":rails_root/public/assets/resumes/:id/:style/:basename.:extension",
-  :url => ":rails_root/public/assets/resumes/:id/:style/:basename.:extension"
+#validates_attachment_content_type :resume, :content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document), 
+validates_attachment_content_type :resume, :content_type =>['application/pdf'],
+:path => ":rails_root/public/assets/resumes/:id/:style/:basename.:extension",
+:url => ":rails_root/public/assets/resumes/:id/:style/:basename.:extension"
 
-  def timeout_in
-      2.minute 
-  end
+
+# def resume_attached?
+#   self.resume.file?
+# end
+
+  # def timeout_in
+  #     2.minute 
+  # end
 
 end
